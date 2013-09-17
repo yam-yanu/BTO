a820b104f03967eb273fc5399d86a1a1ad0fc51e
yam-yanu <you1095@gmail.com>
2013-09-17 14:33:55 +0900

    Initial Commit

---
+++ BTO/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BTO
+//
+//  Created by ami on 2013/09/17.
+//  Copyright (c) 2013年 AMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BTO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BTO
+//
+//  Created by ami on 2013/09/17.
+//  Copyright (c) 2013年 AMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
