1816918b10590a56e28e03166552a94e9ed017d1
yam-yanu <you1095@gmail.com>
2013-09-26 18:33:47 +0900

    userdefaultsファイルの追加

---
+++ BTO/UserDefaultAcceess.h
@@ -0,0 +1,13 @@
+//
+//  UserDefaultAcceess.h
+//  BTO
+//
+//  Created by ami on 2013/09/26.
+//  Copyright (c) 2013年 AMI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserDefaultAcceess : NSObject
+
+@end
