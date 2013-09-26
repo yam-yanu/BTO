d86c1924d4ea1998bcb3ada7c3191ec0041c7b34
yam-yanu <you1095@gmail.com>
2013-09-26 18:33:47 +0900

    userdefaultsファイルの追加＋コンフリクトの解消
    Conflicts:
    	BTO.xcodeproj/project.pbxproj
    
    BTO.xcodeproj/project.xcworkspace/xcuserdata/ami.xcuserdatad/UserInterfa
    ceState.xcuserstate

---
+++ BTO/UserDefaultAcceess.m
@@ -0,0 +1,13 @@
+//
+//  UserDefaultAcceess.m
+//  BTO
+//
+//  Created by ami on 2013/09/26.
+//  Copyright (c) 2013年 AMI. All rights reserved.
+//
+
+#import "UserDefaultAcceess.h"
+
+@implementation UserDefaultAcceess
+
+@end
