11dbb3eb9d7672ab9624da0df56145c0c25771cf
yam-yanu <you1095@gmail.com>
2013-11-22 21:48:35 +0900

    ルートビューの注意書き追加

--- BTO/RootViewController.m
+++ BTO/RootViewController.m
@@ -29,16 +29,15 @@ BOOL alertFinished;
 {
     [super viewDidLoad];
     
-//    //タイトルラベルを追加
-//	UILabel* title =[[UILabel alloc]initWithFrame:self.view.bounds];
-//	title.text = @"BLACK THUNDER OJISAN！";
-//    title.textAlignment = NSTextAlignmentCenter;
-//	title.backgroundColor = [UIColor whiteColor];
-//	title.textColor = [UIColor blackColor];
-//	title.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-//	[self.view addSubview:title];
-    
+
+    //背景画像を追加
     UIImage *backgroundImage = [UIImage imageNamed:@"BTO.jpg"];
+    CGRect r = [[UIScreen mainScreen] bounds];
+    if (r.size.height == 480) {
+        // iPhone4 or iPhone4S
+        CGImageRef iphone5Image = CGImageCreateWithImageInRect(backgroundImage.CGImage, CGRectMake(0, 88, r.size.width,r.size.height));
+        backgroundImage = [UIImage imageWithCGImage:iphone5Image];
+    }
     self.view.backgroundColor = [UIColor colorWithPatternImage:backgroundImage];
     
     //探す側のボタン
@@ -53,8 +52,8 @@ BOOL alertFinished;
     //BTO側にいくボタン
     UIButton *bto = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     bto.frame = CGRectMake(200, 300, 100, 30);
-    search.backgroundColor = [UIColor whiteColor];
-    [bto setTitle:@"BTO" forState:UIControlStateNormal];
+    bto.backgroundColor = [UIColor whiteColor];
+    [bto setTitle:@"BTOになる" forState:UIControlStateNormal];
     [bto addTarget:self
             action:@selector(bto:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:bto];
@@ -64,6 +63,7 @@ BOOL alertFinished;
     if([UserDefaultAcceess getMyID] == 0){
         SSGentleAlertView* alert = [SSGentleAlertView new];
         alert.delegate = self;
+        alert.tag = 1;
         alert.title = @"通信エラー";
         alert.message = @"最初に通信エラーを起こしたためうまく動作できません\nもう一度通信を行います。";
         [alert addButtonWithTitle:@"OK"];
@@ -85,31 +85,60 @@ BOOL alertFinished;
 }
 
 -(void)bto:(UIButton*)button{
-    //MakeBTOViewControllerに遷移
-    UIViewController *bto = [[MakeBTOViewController alloc]init];
-    bto.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
-    [self presentViewController:bto animated:YES completion:^ {
-        [UserDefaultAcceess ChangeState:0];
-    }];
+    if([UserDefaultAcceess getMyName]){
+        //MakeBTOViewControllerに遷移
+        UIViewController *bto = [[MakeBTOViewController alloc]init];
+        bto.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+        [self presentViewController:bto animated:YES completion:^ {
+            [UserDefaultAcceess ChangeState:0];
+        }];
+    }else{
+        SSGentleAlertView* alert = [SSGentleAlertView new];
+        alert.delegate = self;
+        alert.tag = 2;
+        alert.title = @"注意";
+        alert.message = @"おっさんになっている間はGPSをONにする必要があります。\nまた、現在位置が10分おきに\n他人に伝わります。\nそれでもおっさんになりますか？";
+        [alert addButtonWithTitle:@"ならない"];
+        [alert addButtonWithTitle:@"なる"];
+        alert.cancelButtonIndex = 0;
+        [alert show];
+    }
     
 }
 
 // アラートのボタンが押された時に呼ばれるデリゲート
 -(void)alertView:(UIAlertView*)alertView
 clickedButtonAtIndex:(NSInteger)buttonIndex {
-    [SVProgressHUD show];
-    [UserDefaultAcceess LaunchApp];
-    if([UserDefaultAcceess getMyID] == 0){
-        SSGentleAlertView* alert = [SSGentleAlertView new];
-        alert.delegate = self;
-        alert.title = @"通信エラー";
-        alert.message = @"最初に通信エラーを起こしたため\nうまく動作できません\nもう一度通信を行います";
-        [alert addButtonWithTitle:@"OK"];
-        alert.cancelButtonIndex = 0;
-        [alert show];
+    if(alertView.tag == 1){
+        [SVProgressHUD show];
+        [UserDefaultAcceess LaunchApp];
+        if([UserDefaultAcceess getMyID] == 0){
+            SSGentleAlertView* alert = [SSGentleAlertView new];
+            alert.delegate = self;
+            alert.tag = 1;
+            alert.title = @"通信エラー";
+            alert.message = @"最初に通信エラーを起こしたため\nうまく動作できません\nもう一度通信を行います";
+            [alert addButtonWithTitle:@"OK"];
+            alert.cancelButtonIndex = 0;
+            [alert show];
+        }
+        [SVProgressHUD dismiss];
+    }else if(alertView.tag == 2){
+        if(buttonIndex == 1){
+            //MakeBTOViewControllerに遷移
+            UIViewController *bto = [[MakeBTOViewController alloc]init];
+            bto.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+            [self presentViewController:bto animated:YES completion:^ {
+                [UserDefaultAcceess ChangeState:0];
+            }];
+        }
     }
-    [SVProgressHUD dismiss];
     
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
 }
 
 
